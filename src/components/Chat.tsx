9840268a438a19dcc1ac1bd0a38579929da85982
Pratham Das <116100433+Prathamdas3@users.noreply.github.com>
2023-10-27 01:56:46 +0530

    (fix): fixed the chat component type issue

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -10,9 +10,18 @@ import {
   orderBy,
 } from 'firebase/firestore';
 import { auth, db } from '@/config/firebase';
+
+type Message = {
+  text: string;
+  createdAt: any;
+  user: string;
+  room: string;
+  id: string;
+};
+
 export default function Page({ code }: ChatProp) {
   const [newMessage, setNewMessage] = useState<string>('');
-  const [message, setMessage] = useState([]);
+  const [message, setMessage] = useState<Array<Message>>([]);
   const messageRef = collection(db, 'messages');
 
   useEffect(() => {
@@ -22,9 +31,9 @@ export default function Page({ code }: ChatProp) {
       orderBy('createdAt')
     );
     const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
-      let messages = [];
+      let messages: Array<Message> = [];
       snapshot.forEach((doc) => {
-        messages.push({ ...doc.data(), id: doc.id });
+        messages.push({ ...doc.data(), id: doc.id } as Message);
       });
 
       setMessage(messages);
@@ -33,7 +42,7 @@ export default function Page({ code }: ChatProp) {
     return () => unsubscribe();
   }, [messageRef, code]);
 
-  const handleSubmit = async (e: any) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (newMessage === '') return;
     await addDoc(messageRef, {
@@ -51,12 +60,13 @@ export default function Page({ code }: ChatProp) {
         <h1>Welcome to: {code}</h1>
       </div>
       <div className="order-2 h-[75vh] my-4 border border-gray-700 rounded-lg p-4">
-        {message.map((message) => (
-          <h1 key={message.id} className="text-lg text-gray-300">
-            {message.user}
-            <span className="text-sm text-white"> {message.text} </span>
-          </h1>
-        ))}
+        {message &&
+          message.map((message: Message) => (
+            <h1 key={message.id} className="text-lg text-gray-300">
+              {message.user}
+              <span className="text-sm text-white"> {message.text} </span>
+            </h1>
+          ))}
       </div>
       <form
         onSubmit={handleSubmit}
