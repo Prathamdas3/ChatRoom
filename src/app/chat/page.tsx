9874c74bc9bfa38b6520d3c513701bcb5ba2b0b1
Pratham Das <116100433+Prathamdas3@users.noreply.github.com>
2023-10-27 01:09:56 +0530

    (fix): chat releted types are fixed

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,12 +2,15 @@
 import React, { useState, useRef } from 'react';
 import Chat from '@/components/Chat';
 export default function Page() {
-  const [room, setRoom] = useState(null);
-  const roomInputRef = useRef(null);
+  const [room, setRoom] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const roomInputRef = useRef<HTMLInputElement>(null);
 
-  const handleChat = (e: any) => {
-    e.preventDefault();
-    document.getElementById('my_modal_1').showModal();
+  const handleJoinRoom = () => {
+    if (roomInputRef.current) {
+      setRoom(roomInputRef.current.value);
+    }
+    setIsModalOpen(false);
   };
 
   return (
@@ -18,39 +21,45 @@ export default function Page() {
         </div>
       ) : (
         <div className="container mx-auto flex flex-col items-center justify-center min-h-screen py-2 gap-4">
-          <button className="btn" onClick={handleChat}>
+          <button className="btn" onClick={() => setIsModalOpen(true)}>
             Join Chat
           </button>
-          <dialog id="my_modal_1" className="modal">
-            <div className="modal-box flex flex-col gap-2">
-              <h3 className="font-bold text-lg">Join chatroom</h3>
-              <label className="mx-2 text-sm" htmlFor="room_id">
-                Enter room code
-              </label>
-              <input
-                type="text"
-                placeholder="Type code "
-                id="room_id"
-                className="input input-bordered w-full max-w-xs"
-                ref={roomInputRef}
-              />
+          {isModalOpen && (
+            <div id="my_modal_1" className="modal">
+              <div className="modal-box flex flex-col gap-2">
+                <h3 className="font-bold text-lg">Join chatroom</h3>
+                <label className="mx-2 text-sm" htmlFor="room_id">
+                  Enter room code
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type code "
+                  id="room_id"
+                  className="input input-bordered w-full max-w-xs"
+                  ref={roomInputRef}
+                />
 
-              <div className="modal-action">
-                <form method="dialog " className="flex gap-2">
-                  <button type="submit" className="btn btn-error">
-                    Close
-                  </button>
-                  <button
-                    type="submit"
-                    className="btn btn-success"
-                    onClick={() => setRoom(roomInputRef.current.value)}
-                  >
-                    Join
-                  </button>
-                </form>
+                <div className="modal-action">
+                  <form method="dialog " className="flex gap-2">
+                    <button
+                      type="submit"
+                      className="btn btn-error"
+                      onClick={() => setIsModalOpen(false)}
+                    >
+                      Close
+                    </button>
+                    <button
+                      type="submit"
+                      className="btn btn-success"
+                      onClick={handleJoinRoom}
+                    >
+                      Join
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
-          </dialog>
+          )}
         </div>
       )}
     </div>
